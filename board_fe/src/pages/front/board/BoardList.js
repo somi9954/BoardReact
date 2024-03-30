773673732353ce65dcc390b4ceb0dbfd69202b2e
somi9954 <jolh999@gmail.com>
2024-03-31 00:54:52 +0900

    :memo: 게시판 리스트 구현중

--- board_fe/src/pages/front/board/BoardList.js
+++ board_fe/src/pages/front/board/BoardList.js
@@ -1,26 +1,20 @@
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
 import { OuterBox } from '../../../components/commons/OutlineStyle';
 import { MainTitle } from '../../../components/commons/TitleStyle';
 import BoardListContainer from '../../../containers/board/BoardListContainer';
 
 const BoardList = () => {
   const { t } = useTranslation();
-  const [bId, setBId] = useState('');
-
-  const handleChangeBId = (newBId) => {
-    setBId(newBId);
-  };
 
   return (
     <>
       <Helmet>
-        <title>{t(`${bId}`)}</title>
+        <title>{t('리스트')}</title>
       </Helmet>
       <OuterBox>
-        <MainTitle color="#ff4910">{t(`${bId}`)}</MainTitle>
-        <BoardListContainer bId={bId} onChangeBId={handleChangeBId} />
+        <MainTitle color="#ff4910">{t('리스트')}</MainTitle>
+        <BoardListContainer />
       </OuterBox>
     </>
   );
