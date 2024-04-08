d3fd99ec7c39fbdd819b987be29f972482a5c3ec
somi9954 <jolh999@gmail.com>
2024-04-08 16:39:31 +0900

    :memo: 수정 페이지 타이틀과 메인타이틀 데이터 가져와서 정의

--- board_fe/src/pages/front/board/BoardUpdate.js
+++ board_fe/src/pages/front/board/BoardUpdate.js
@@ -2,21 +2,49 @@ import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { OuterBox } from '../../../components/commons/OutlineStyle';
 import { MainTitle } from '../../../components/commons/TitleStyle';
+import responseView from '../../../api/board/boardView';
+import { useEffect, useState } from 'react';
 import BoardUpdateContainer from '../../../containers/board/BoardUpdateContainer';
 
-const BoardWrite = () => {
+const BoardView = () => {
   const { t } = useTranslation();
+  const [pageTitle, setPageTitle] = useState(''); // 페이지 제목을 상태로 저장
+
+  useEffect(() => {
+    const getSeqFromURL = () => {
+      const parts = window.location.pathname.split('/');
+      return parts[parts.length - 1];
+    };
+
+    const seq = getSeqFromURL();
+    responseView(seq)
+      .then((responseData) => {
+        const { success, data } = responseData;
+        if (success) {
+          const { subject } = data;
+          setPageTitle(subject);
+        } else {
+          console.error('Error fetching data:', responseData);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <>
       <Helmet>
-        <title>{t('커뮤니티 글쓰기 수정')}</title>
+        <title color="#ff4910">{t(pageTitle)} 수정</title>
       </Helmet>
       <OuterBox>
-        <MainTitle color="#ff4910">{t('커뮤니티 글쓰기 수정')}</MainTitle>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <MainTitle color="#ff4910">{t(pageTitle)} 수정</MainTitle>
+        </div>
         <BoardUpdateContainer />
       </OuterBox>
     </>
   );
 };
 
-export default BoardWrite;
+export default BoardView;
