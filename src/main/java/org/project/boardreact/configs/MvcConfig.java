9d2e926b085d75352fbac5ff798fd0b3be355de3
somi9954 <jolh999@gmail.com>
2024-04-16 19:31:34 +0900

    :memo: AWS Cors 수정 중

--- src/main/java/org/project/boardreact/configs/MvcConfig.java
+++ src/main/java/org/project/boardreact/configs/MvcConfig.java
@@ -32,6 +32,7 @@ public class MvcConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://3.36.76.61:3000");
+        config.addAllowedOrigin("http://3.36.76.61:8080");
         config.addAllowedOrigin("http://localhost:3000");
 
         // 메서드 및 헤더 제한
